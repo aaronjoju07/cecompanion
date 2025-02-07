82999e331793bb948fe800d4fce0bf0a272e0ed7
aaronjoju07 <aaronjoju07@gmail.com>
2025-02-07 18:27:58 +0530

    score service created

---
+++ cecompanion/src/main/java/com/score/cecompanion/CecompanionApplication.java
@@ -0,0 +1,13 @@
+package com.score.cecompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CecompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CecompanionApplication.class, args);
+	}
+
+}
---
+++ cecompanion/src/test/java/com/score/cecompanion/CecompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.score.cecompanion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CecompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
